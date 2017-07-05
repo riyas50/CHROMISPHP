6e4b32d8b99eb401c58355fe608de9eb78d81b20
riyas <riyas.a@ADS.LOCAL>
2017-07-05 17:13:38 +0400

    main page change | renamed heading to CHROMIS BACKOFFICE | new page

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 Working with below versions:
 Chromis POS	0.58.5.7a | Server type: MySQL | Server version: 5.6.26 - MySQL Community Server (GPL)
+Chromis POS	0.58.5.7a | Server type: MariaDB | Server version: 10.1.21-MariaDB - mariadb.org binary distribution
+Apache/2.4.25 (Win32) OpenSSL/1.0.2j PHP/5.6.30
+PHP version: 5.6.30
+PHP extension: mysqli | curl | mbstring
+
---
+++ productpurchase.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php
+        include('general.php');
+        putLinks();
+    ?>
+   <title>Chromis PHP - Categories</title>
+  </head>
+  <body>
+
+  
+  
+<header class="site__header island">
+  <div class="wrap">
+   <span id="animationSandbox" style="display: block;"  class="tada animated">
+   <h1 class="site__title mega text-center">Page Template</h1>
+   </span>
+  </div>
+  </header>
+
+<form action="categories.php" method="post" enctype="multipart/form-data">
+ <div class="row">
+ <div class="form-group">
+ <div class="col-lg-4"></div>
+ <div class="col-lg-4 text-right">
+          <a class="glyphicon glyphicon-home" style="font-size:30px;color:orange" href="/chromisphp/"></a> 
+ <div class="input-group">
+    <div class="input-group-addon">Template</div>
+    <input type="text" class="form-control" name="category" id="inlineFormInputGroup" placeholder="Template" required>
+    </div>
+    <br />  
+     <button type="submit" class="btn btn-success" id="btnSave" name="Save">Save</button>
+     <button type="submit" class="btn btn-warning" id="btnRemove" name="Remove">Remove</button>
+     <button type="submit" class="btn btn-info" id="btnRefresh" name="Refresh" formnovalidate>Refresh</button>
+ </div>
+ <div class="col-lg-4"></div>
+ </div>
+ </div>
+ </form>
+<br />
+<div class="row">
+<div class="col-lg-4"></div>
+<div class="col-lg-4">
+<div class="col-lg-4"></div>
+</div>
+</div>
+
+<?php 
+    if (isset($_POST['Save']))
+    {
+        //php code here
+    }
+
+if (isset($_POST['Remove']))
+    {
+        //php code here
+    }
+
+?>
+
+	<?php
+    //include('general.php');
+    putScripts();
+    stickfooter();
+?>
+<!--
+-->  </body>
+</html>
