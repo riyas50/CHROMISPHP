c2d90c2424a32d91e2704dbc6f34502fc1d7e91a
riyas <riyas.a@ADS.LOCAL>
2017-11-19 16:10:39 +0400

    begun to add receipt print functionality for estimate

---
+++ printestimate.php
@@ -0,0 +1,41 @@
+<?php
+/* Change to the correct path if you copy this example! */
+require '/vendor/mike42/escpos-php/autoload.php';
+use Mike42\Escpos\Printer;
+use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
+/**
+ * Assuming your printer is available at LPT1,
+ * simpy instantiate a WindowsPrintConnector to it.
+ *
+ * When troubleshooting, make sure you can send it
+ * data from the command-line first:
+ *  echo "Hello World" > LPT1
+ */
+try {
+    $connector = new WindowsPrintConnector("LPT1");
+    
+    // A FilePrintConnector will also work, but on non-Windows systems, writes
+    // to an actual file called 'LPT1' rather than giving a useful error.
+    // $connector = new FilePrintConnector("LPT1");
+    /* Print a "Hello world" receipt" */
+    $printer = new Printer($connector);
+    $barcode="8901180228128";
+    $desc="Faber Castell Plastic Crayons 12+2";
+    $price="1156.82";
+    $qty="111";
+    $total="1111.00";
+    $printer -> setJustification(Printer::JUSTIFY_CENTER);
+    $printer -> text("Estimate\n");
+    $printer -> setJustification(Printer::JUSTIFY_LEFT);
+    $printer -> text(str_repeat("=",42) . "\n");
+    $printer -> text(str_pad("Qty",3) . " " . str_pad("Item Details",22)  . " " . str_pad("Price",7," ",STR_PAD_RIGHT) . " " . str_pad("Total",7," ",STR_PAD_RIGHT) . "\n");
+    $printer -> text(str_repeat("=",42) . "\n");
+    $printer -> text(str_pad($qty,3) . " " . str_pad(trim(substr($desc,0,20)),22) . " " . str_pad($price,7," ",STR_PAD_RIGHT) . " " . str_pad($total,7," ",STR_PAD_RIGHT) . "\n");
+    $printer -> text(str_pad($barcode,17," ",STR_PAD_LEFT) . "\n");
+    $printer -> cut();
+    /* Close printer */
+    $printer -> close();
+} catch (Exception $e) {
+    echo "Couldn't print to this printer: " . $e -> getMessage() . "\n";
+}
+//   $connector = new WindowsPrintConnector("EPSON TM-T88IV Receipt");
