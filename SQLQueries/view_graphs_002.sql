0a448d7938e6d314c0258e08ab3b354b608c67e5
Riyas Aboobaker <riyas50@gmail.com>
2018-06-28 06:47:24 +0400

    dashboard shifted to analytics menu

---
+++ SQLQueries/view_graphs_002.sql
@@ -0,0 +1,181 @@
+﻿-- 
+-- Set character set the client will use to send SQL statements to the server
+--
+SET NAMES 'utf8';
+
+--
+-- Set default database
+--
+USE chromispos;
+
+--
+-- Create view `view_yearly_sales_all_cash`
+--
+CREATE 
+	DEFINER = 'root'@'localhost'
+VIEW view_yearly_sales_all_cash
+AS
+SELECT
+  SUM(`b`.`INVAMOUNT`) AS `INVAMOUNTAB`,
+  YEAR(`b`.`INVDATE`) AS `INVDATEAB`
+FROM `view_all_invoices` `b`
+GROUP BY `b`.`INVAMOUNT`,
+         YEAR(`b`.`INVDATE`);
+
+--
+-- Create view `view_yearly_sales_all_customers`
+--
+CREATE 
+	DEFINER = 'root'@'localhost'
+VIEW view_yearly_sales_all_customers
+AS
+SELECT
+  SUM(`a`.`INVAMOUNT`) AS `INVAMOUNTAB`,
+  YEAR(`a`.`INVDATE`) AS `INVDATEAB`
+FROM `view_all_cash_customers` `a`
+GROUP BY `a`.`INVAMOUNT`,
+         YEAR(`a`.`INVDATE`);
+
+--
+-- Create view `view_yearly_sales_all`
+--
+CREATE 
+	DEFINER = 'root'@'localhost'
+VIEW view_yearly_sales_all
+AS
+SELECT
+  `view_yearly_sales_all_customers`.`INVAMOUNTAB` AS `INVAMOUNTAB`,
+  `view_yearly_sales_all_customers`.`INVDATEAB` AS `INVDATEAB`
+FROM `view_yearly_sales_all_customers` UNION ALL SELECT
+  `view_yearly_sales_all_cash`.`INVAMOUNTAB` AS `INVAMOUNTAB`,
+  `view_yearly_sales_all_cash`.`INVDATEAB` AS `INVDATEAB`
+FROM `view_yearly_sales_all_cash`;
+
+--
+-- Create view `view_monthly_sales_all_customers`
+--
+CREATE 
+	DEFINER = 'root'@'localhost'
+VIEW view_monthly_sales_all_customers
+AS
+SELECT
+  SUM(`a`.`INVAMOUNT`) AS `INVAMOUNTAB`,
+  `a`.`INVDATE` AS `INVDATEAB`
+FROM `view_all_cash_customers` `a`
+GROUP BY `a`.`INVAMOUNT`,
+         YEAR(`a`.`INVDATE`),
+         MONTH(`a`.`INVDATE`)
+ORDER BY `INVDATEAB`;
+
+--
+-- Create view `view_current_month_cust_sales`
+--
+CREATE 
+	DEFINER = 'root'@'localhost'
+VIEW view_current_month_cust_sales
+AS
+SELECT
+  SUM(`view_monthly_sales_all_customers`.`INVAMOUNTAB`) AS `INVAMOUNT`,
+  DATE_FORMAT(`view_monthly_sales_all_customers`.`INVDATEAB`, '%M-%Y') AS `INVDATE`
+FROM `view_monthly_sales_all_customers`
+WHERE (MONTH(`view_monthly_sales_all_customers`.`INVDATEAB`) = MONTH(NOW()))
+GROUP BY YEAR(`view_monthly_sales_all_customers`.`INVDATEAB`),
+         MONTH(`view_monthly_sales_all_customers`.`INVDATEAB`);
+
+--
+-- Create view `view_monthly_sales_all_cash`
+--
+CREATE 
+	DEFINER = 'root'@'localhost'
+VIEW view_monthly_sales_all_cash
+AS
+SELECT
+  SUM(`b`.`INVAMOUNT`) AS `INVAMOUNTAB`,
+  `b`.`INVDATE` AS `INVDATEAB`
+FROM `view_all_invoices` `b`
+GROUP BY `b`.`INVAMOUNT`,
+         YEAR(`b`.`INVDATE`),
+         MONTH(`b`.`INVDATE`)
+ORDER BY `INVDATEAB`;
+
+--
+-- Create view `view_monthly_sales_all`
+--
+CREATE 
+	DEFINER = 'root'@'localhost'
+VIEW view_monthly_sales_all
+AS
+SELECT
+  SUM(`view_monthly_sales_all_customers`.`INVAMOUNTAB`) AS `INVAMOUNT`,
+  DATE_FORMAT(`view_monthly_sales_all_customers`.`INVDATEAB`, '%M-%Y') AS `INVDATE`
+FROM `view_monthly_sales_all_customers`
+GROUP BY YEAR(`view_monthly_sales_all_customers`.`INVDATEAB`),
+         MONTH(`view_monthly_sales_all_customers`.`INVDATEAB`) UNION ALL SELECT
+  SUM(`view_monthly_sales_all_cash`.`INVAMOUNTAB`) AS `INVAMOUNT`,
+  DATE_FORMAT(`view_monthly_sales_all_cash`.`INVDATEAB`, '%M-%Y') AS `INVDATE`
+FROM `view_monthly_sales_all_cash`
+GROUP BY YEAR(`view_monthly_sales_all_cash`.`INVDATEAB`),
+         MONTH(`view_monthly_sales_all_cash`.`INVDATEAB`);
+
+--
+-- Create view `view_current_month_cash_sales`
+--
+CREATE 
+	DEFINER = 'root'@'localhost'
+VIEW view_current_month_cash_sales
+AS
+SELECT
+  SUM(`view_monthly_sales_all_cash`.`INVAMOUNTAB`) AS `INVAMOUNT`,
+  DATE_FORMAT(`view_monthly_sales_all_cash`.`INVDATEAB`, '%M-%Y') AS `INVDATE`
+FROM `view_monthly_sales_all_cash`
+WHERE (MONTH(`view_monthly_sales_all_cash`.`INVDATEAB`) = MONTH(NOW()))
+GROUP BY YEAR(`view_monthly_sales_all_cash`.`INVDATEAB`),
+         MONTH(`view_monthly_sales_all_cash`.`INVDATEAB`);
+
+--
+-- Create view `view_yearly_total_sales`
+--
+CREATE 
+	DEFINER = 'root'@'localhost'
+VIEW view_yearly_total_sales
+AS
+SELECT
+  SUM((`ticketlines`.`PRICE` * `ticketlines`.`UNITS`)) AS `ACTUAL_SALES_VALUE`,
+  YEAR(`receipts`.`DATENEW`) AS `oYear`
+FROM (((`ticketlines`
+  JOIN `products`
+    ON ((`products`.`ID` = `ticketlines`.`PRODUCT`)))
+  JOIN `receipts`
+    ON ((`receipts`.`ID` = `ticketlines`.`TICKET`)))
+  JOIN `tickets`
+    ON (((`tickets`.`ID` = `ticketlines`.`TICKET`)
+    AND (`tickets`.`TICKETTYPE` = 0))))
+WHERE `ticketlines`.`TICKET` IN (SELECT
+    `receipts`.`ID`
+  FROM `receipts`)
+GROUP BY YEAR(`receipts`.`DATENEW`)
+ORDER BY YEAR(`receipts`.`DATENEW`);
+
+--
+-- Create view `view_yearly_total_profit`
+--
+CREATE 
+	DEFINER = 'root'@'localhost'
+VIEW view_yearly_total_profit
+AS
+SELECT
+  (SUM((`ticketlines`.`PRICE` * `ticketlines`.`UNITS`)) - SUM((`ticketlines`.`UNITS` * `products`.`PRICEBUY`))) AS `ACTUAL_PROFIT`,
+  YEAR(`receipts`.`DATENEW`) AS `oYear`
+FROM (((`ticketlines`
+  JOIN `products`
+    ON ((`products`.`ID` = `ticketlines`.`PRODUCT`)))
+  JOIN `receipts`
+    ON ((`receipts`.`ID` = `ticketlines`.`TICKET`)))
+  JOIN `tickets`
+    ON (((`tickets`.`ID` = `ticketlines`.`TICKET`)
+    AND (`tickets`.`TICKETTYPE` = 0))))
+WHERE `ticketlines`.`TICKET` IN (SELECT
+    `receipts`.`ID`
+  FROM `receipts`)
+GROUP BY YEAR(`receipts`.`DATENEW`)
+ORDER BY YEAR(`receipts`.`DATENEW`);
--- index.php
+++ index.php
@@ -3,7 +3,6 @@
   <head>
     <?php
         include('general.php');
-        include('index_charts.php');
         putScripts();
         putLinks();
     ?>
@@ -33,7 +32,7 @@
                     <li><a href="/chromisphp/productsearch.php">Search</a></li>
                     <li><a href="#">New Product</a></li>
                     <li><a href="/chromisphp/productpurchase.php">Purchase</a></li>
-                    <li><a href="#">Distribution</a></li>
+                    <li><a href="./marginsearch.php">Distribution</a></li>
                 </ul>
             </li>
             <li class="dropdown">
@@ -62,233 +61,27 @@
                     <li><a href="./Report_CashReceiptsItems.php">Cash Receipts Product Search</a></li>
                 </ul>
             </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Analytics <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                    <li><a href="./dashboard.php">Dashboard</a></li>
+                    <li><a href="#">Reserved</a></li>
+                    <li><a href="#">Reserved</a></li>
+                    <li><a href="#">Reserved</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Reserved</a></li>
+                    <li><a href="#">Reserved</a></li>
+                </ul>
+            </li>
         </ul>
     </div><!-- /.navbar-collapse -->
 </nav>
 
-<header class="site__header island">
-  <div class="wrap">
-   <span id="animationSandbox" style="display: block; color:red;"  class="tada animated">
-   <h1 class="site__title mega text-center">Grand Dashboard</h1>
-   </span>
-  </div>
-  </header>
-
 <div class="container-fluid">
-    <div class="row">
-        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 text-center">
-            <canvas id="YearlyTotalSalesChart" width="100" height="100"></canvas>
-        </div>    
-        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 text-center">
-            <canvas id="YearlyCashSalesChart" width="150" height="150"></canvas>
-        </div>
-        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 text-center">
-            <canvas id="YearlyCustSalesChart" width="150" height="150"></canvas>
-        </div>
-        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 text-center">
-            <canvas id="CurrMonthCashSalesChart" width="150" height="150"></canvas>
-        </div>
-        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 text-center">
-            <canvas id="CurrMonthCustSalesChart" width="100" height="100"></canvas>
-        </div>
-        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 text-center">
-            <canvas id="TotalYearlyProfitChart" width="100" height="100"></canvas>
-        </div>
-    </div>
+
 </div> <!-- container -->
 
-    <?php //Yearly Total Sales
-        TotalYearlySales();                                         
-    ?>
-    
-    <script>
-        var ctx = document.getElementById('YearlyTotalSalesChart').getContext('2d');
-        var chart = new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'bar',
-            // The data for our dataset
-            data: {
-                labels: <?php echo json_encode($outputYears);?>,
-                datasets: [{
-                    label: "Yearly Total Sales",
-                    backgroundColor: 'rgb(255,23,68)',
-                    borderColor: 'rgb(255,23,68)',
-                    data: <?php echo json_encode($outputInvAmount);?>,
-                }]
-            },
-            // Configuration options go here
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
-</script>
 
-    <?php //Yearly Total Sales
-        TotalYearlyProfit();                                         
-    ?>
-    
-    <script>
-        var ctx = document.getElementById('TotalYearlyProfitChart').getContext('2d');
-        var chart = new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'bar',
-            // The data for our dataset
-            data: {
-                labels: <?php echo json_encode($outputYears);?>,
-                datasets: [{
-                    label: "Yearly Total Profit",
-                    backgroundColor: 'rgb(118,255,3)',
-                    borderColor: 'rgb(118,255,3)',
-                    data: <?php echo json_encode($outputInvAmount);?>,
-                }]
-            },
-            // Configuration options go here
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
-</script>
-
-    <?php //Yearly Sales Cash
-        YearlySalesCash();                                         
-    ?>
-    
-    <script>
-        var ctx = document.getElementById('YearlyCashSalesChart').getContext('2d');
-        var chart = new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'bar',
-            // The data for our dataset
-            data: {
-                labels: <?php echo json_encode($outputYears);?>,
-                datasets: [{
-                    label: "Yearly Cash Sales",
-                    backgroundColor: 'rgb(255,235,59)',
-                    borderColor: 'rgb(255,235,59)',
-                    data: <?php echo json_encode($outputInvAmount);?>,
-                }]
-            },
-            // Configuration options go here
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
-</script>
-
-    <?php //Yearly Sales Customers
-        YearlySalesCustomers();                                         
-    ?>
-
-    <script>
-        var ctx = document.getElementById('YearlyCustSalesChart').getContext('2d');
-        var chart = new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'bar',
-            // The data for our dataset
-            data: {
-                labels: <?php echo json_encode($outputYears);?>,
-                datasets: [{
-                    label: "Yearly Customer Sales",
-                    backgroundColor: 'rgb(213,0,249)',
-                    borderColor: 'rgb(213,0,249)',
-                    data: <?php echo json_encode($outputInvAmount);?>,
-                }]
-            },
-            // Configuration options go here
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }                
-            }
-        });
-</script>
-
-    <?php   //current month cash Sales 
-            //previous years vs current year
-        CurrentMonthCashSales();                                         
-    ?>
-
-    <script>
-        var ctx = document.getElementById('CurrMonthCashSalesChart').getContext('2d');
-        var chart = new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'bar',
-            // The data for our dataset
-            data: {
-                labels: <?php echo json_encode($outputYears);?>,
-                datasets: [{
-                    label: "This Month Cash Sales",
-                    backgroundColor: 'rgb(21,101,192)',
-                    borderColor: 'rgb(21,101,192)',
-                    data: <?php echo json_encode($outputInvAmount);?>,
-                }]
-            },
-            // Configuration options go here
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }                
-            }
-        });
-</script>
-    <?php   //Current month sales customers 
-            //Previous Years vs current year
-        CurrentMonthCustSales();                                         
-    ?>
-
-    <script>
-        var ctx = document.getElementById('CurrMonthCustSalesChart').getContext('2d');
-        var chart = new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'bar',
-            // The data for our dataset
-            data: {
-                labels: <?php echo json_encode($outputYears);?>,
-                datasets: [{
-                    label: "This Month Cust Sales",
-                    backgroundColor: 'rgb(245,0,87)',
-                    borderColor: 'rgb(245,0,87)',
-                    data: <?php echo json_encode($outputInvAmount);?>,
-                }]
-            },
-            // Configuration options go here
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }                
-            }
-        });
-</script>
 
 
 
